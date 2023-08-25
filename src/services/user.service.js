03e42b2a34fd3e6f3e75ea4a3dcefe84abc0d3af
Helio Morais <heliogov20@gmail.com>
2023-08-25 18:15:10 -0300

    Requisito 5

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -5,10 +5,11 @@ const CONFLICT_CODE = 409;
 const CREATED_CODE = 201;
 const CONFLICT_MESSAGE = 'User already registered';
 
-const getAll = async () => {
-  const users = await User.findAll();
-  return users;
+const getAll = async (options) => {
+    const users = await User.findAll(options);
+    return users.map(({ id, displayName, email, image }) => ({ id, displayName, email, image }));
 };
+
 const getOne = async (options) => {
   const users = await User.findOne(options);
   return users;
@@ -35,8 +36,14 @@ const saveOne = async (data) => {
   }
 };
 
+const getUserById = async (id) => {
+    const user = await User.findOne({ where: { id } });
+    return user;
+  };
+
 module.exports = {
   getAll,
   getOne,
   saveOne,
+  getUserById,
 };
