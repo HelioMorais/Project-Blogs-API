c59af2ea96e3a5cd3b8d9d22a6b653a0c94eb71a
Helio Morais <heliogov20@gmail.com>
2023-08-25 16:29:34 -0300

    Requisito 1

--- src/migrations/20230825182453-create-user.js
+++ src/migrations/20230825182453-create-user.js
@@ -2,20 +2,33 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER
+      },
+      display_name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        unique: true,
+        allowNull: false,
+        type: Sequelize.STRING
+     },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+     },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING
+     }
+    });
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('users');
   }
 };
---
+++ src/migrations/20230825185521-create-post-category.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+     await queryInterface.createTable('posts_categories', {
+      post_id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      category_id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('posts_categories');
+  }
+};
